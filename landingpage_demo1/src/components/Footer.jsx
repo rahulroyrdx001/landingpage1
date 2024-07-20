b64b152e07d98e27b900f8bdb61fec161575c3a9
rahulroyrdx001 <rahulroyrdx001@gmail.com>
2024-07-20 23:02:58 +0530

    latest update

--- landingpage_demo1/src/components/Footer.jsx
+++ landingpage_demo1/src/components/Footer.jsx
@@ -3,7 +3,28 @@ import style from "../style/footer.module.css";
 export default function Footer() {
   return (
     <>
-      <div className={style.footerbody}>Footer</div>
+      <div className={style.footerbody}>
+        <div className={style.textarea}>
+          <h3>The transition CDN-dCDN</h3>
+          <p>
+            FanCloud pioneers a new era of connectivity with its decentralised
+            content delivery network (dCDN),
+          </p>
+          <p>
+            leveraging a global network of community-powered nodes. By
+            harnessing a decentralised{" "}
+          </p>
+          <p>
+            architecture, dCDN drastically reduces data transfer and compute
+            costs while enhancing load time
+          </p>
+          <p>
+            Join FanCloud’s dCDN network and experience the future of seamless,
+            secure and community-
+          </p>
+          <p>driven consumption</p>
+        </div>
+      </div>
     </>
   );
 }
--- landingpage_demo1/src/components/Home1.jsx
+++ landingpage_demo1/src/components/Home1.jsx
@@ -1,8 +1,137 @@
 import style from "../style/home1.module.css";
+import bgimg from "../assets/bgimg.png";
 export default function Home1() {
   return (
     <>
-      <div className={style.home1body}>Home1</div>
+      <div className={style.home1body}>
+        <div className={style.element1}></div>
+        <div className={style.heading}>
+          <h2>EMPOWERING THE DIGITAL ERA:</h2>
+          <h2>DECENTRALIZING ENTERTAINTMENT</h2>
+        </div>
+        <div className={style.lowerheading}>
+          <p>
+            Join the worlds first DePIN: Revolutionizing content Streaming with
+            faster,Safer
+          </p>
+          <p>and Cheaper Access</p>
+        </div>
+        <div className={style.btn}>
+          <button>Comming soon</button>
+        </div>
+        <div className={style.bgimg}>
+          <img src={bgimg} alt="Background image" />
+        </div>
+        <div className={style.databoxs}>
+          <div className={style.box}>
+            <div className={style.starelement}>
+              <h2>No. of Node Operators</h2>
+              <svg
+                width="47"
+                height="48"
+                viewBox="0 0 47 48"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M23.5 1.5V46.5"
+                  stroke="url(#paint0_linear_1_5410)"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                />
+                <path
+                  d="M1 24L46 24"
+                  stroke="url(#paint1_linear_1_5410)"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                />
+                <defs>
+                  <linearGradient
+                    id="paint0_linear_1_5410"
+                    x1="24"
+                    y1="1.5"
+                    x2="24"
+                    y2="46.5"
+                    gradientUnits="userSpaceOnUse"
+                  >
+                    <stop stop-color="white" stop-opacity="0" />
+                    <stop offset="0.5" stop-color="#1E1E1E" />
+                    <stop offset="1" stop-color="white" stop-opacity="0" />
+                  </linearGradient>
+                  <linearGradient
+                    id="paint1_linear_1_5410"
+                    x1="1"
+                    y1="24"
+                    x2="46"
+                    y2="24"
+                    gradientUnits="userSpaceOnUse"
+                  >
+                    <stop stop-color="white" stop-opacity="0" />
+                    <stop offset="0.505" stop-color="#1E1E1E" />
+                    <stop offset="1" stop-color="white" />
+                  </linearGradient>
+                </defs>
+              </svg>
+            </div>
+            <h1>2000</h1>
+          </div>
+          <div className={style.box}>
+            <div className={style.starelement}>
+              <h2>
+                Data Transferred in TB{" "}
+                <span style={{ fontSize: "15px" }}>(last 30 days)</span>
+              </h2>
+              <svg
+                width="47"
+                height="48"
+                viewBox="0 0 47 48"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M23.5 1.5V46.5"
+                  stroke="url(#paint0_linear_1_5410)"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                />
+                <path
+                  d="M1 24L46 24"
+                  stroke="url(#paint1_linear_1_5410)"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                />
+                <defs>
+                  <linearGradient
+                    id="paint0_linear_1_5410"
+                    x1="24"
+                    y1="1.5"
+                    x2="24"
+                    y2="46.5"
+                    gradientUnits="userSpaceOnUse"
+                  >
+                    <stop stop-color="white" stop-opacity="0" />
+                    <stop offset="0.5" stop-color="#1E1E1E" />
+                    <stop offset="1" stop-color="white" stop-opacity="0" />
+                  </linearGradient>
+                  <linearGradient
+                    id="paint1_linear_1_5410"
+                    x1="1"
+                    y1="24"
+                    x2="46"
+                    y2="24"
+                    gradientUnits="userSpaceOnUse"
+                  >
+                    <stop stop-color="white" stop-opacity="0" />
+                    <stop offset="0.505" stop-color="#1E1E1E" />
+                    <stop offset="1" stop-color="white" />
+                  </linearGradient>
+                </defs>
+              </svg>
+            </div>
+            <h1>12,000</h1>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
